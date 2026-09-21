06f9c78b0702c99d78a780c286d2624efc4da187
Fxmongoose <124783705+Fxmongoose@users.noreply.github.com>
2026-09-21 23:19:46 +0300

    Delete itickets-ios-test/sw.js

--- itickets-ios-test/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='itickets-ios-v2'; const FILES=['./','./index.html','./app.js','./manifest.webmanifest','./assets/chisinau_transport_header.png','./assets/icon_bus.png','./assets/icon_trolleybus.png','./assets/icon_ticket.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
